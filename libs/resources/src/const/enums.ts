629d3278133e142de7d9d416d1e8bb017502c1e9
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2024-03-20 16:38:51 +0300

    feat(libs): add const enums for sort validating asc | desc

---
+++ libs/resources/src/const/enums.ts
@@ -0,0 +1,12 @@
+export enum SORING {
+  NAME_ASC = 'name',
+  SLUG_ASC = 'slug',
+  DESCRIPTION_ASC = 'description',
+  ACTIVE_ASC = 'active',
+  CREATED_ASC = 'createdAt',
+  NAME_DESC = '-name',
+  SLUG_DESC = '-slug',
+  DESCRIPTION_DESC = '-description',
+  ACTIVE_DESC = '-active',
+  CREATED_DESC = '-createdAt',
+}
--- libs/resources/src/index.ts
+++ libs/resources/src/index.ts
@@ -1,3 +1,4 @@
 export * from './dto';
 export * from './swagger';
 export * from './utils';
+export * from './const';
