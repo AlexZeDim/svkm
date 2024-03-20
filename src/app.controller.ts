c16be38ba08133d72f20f87bab54c2c79c440f92
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2024-03-20 15:09:17 +0300

    feat(app): application controller & logic precursor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/createCategory')
+  createCategory(): string {
+    return this.appService.createCategory();
+  }
+
+  @Post('/updateCategory')
+  updateCategory(): string {
+    return this.appService.updateCategory();
+  }
+
+  @Delete('/deleteCategory')
+  deleteCategory(): string {
+    return this.appService.deleteCategory();
+  }
+
+  @Get('/getByIdOrSlug')
+  getByIdOrSlug(query: string): string {
+    return this.appService.getByIdOrSlug(query);
+  }
+
+  @Get('/getByFilter')
+  getByFilter(): string {
+    return this.appService.getByFilter();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { config } from '@svkm/config';
+import process from 'node:process';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnApplicationBootstrap {
+  /**
+   * @description Создать категорию
+   */
+  createCategory(): string {
+    return 'createCategory!';
+  }
+  /**
+   * @description Изменить категорию. Добавить поддержку частичного обновления
+   * модели. Например, возможность изменить только active без
+   * необходимости передавать всю модель. И так для любого поля модели.
+   */
+  updateCategory(): string {
+    return 'updateCategory';
+  }
+  /**
+   * @description Удалить категорию
+   */
+  deleteCategory(): string {
+    return 'deleteCategory!';
+  }
+  /**
+   * @description Получить категорию по id или slug.
+   */
+  getByIdOrSlug(query: string): string {
+    return 'getByIdOrSlug!';
+  }
+  /**
+   * @description Получить массив категорий по фильтру. Без фильтров
+   * по умолчанию отдаются первые две категории отсортированные по дате
+   * создания (поле createdDate). Новые категории идут первыми
+   */
+  getByFilter() {
+    // TODO sort by
+    return 'getByFilter!';
+  }
+
+  onApplicationBootstrap(): void {
+    console.log(config);
+    console.log(process.env.MONGO_CONNECTION);
   }
 }
