667b7a41698d5ab954ea2ebe682c23b7cf532392
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2024-03-20 15:31:57 +0300

    feat(libs): extend category dto with class validators & transformers

--- libs/resources/src/dto/category.dto.ts
+++ libs/resources/src/dto/category.dto.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 import { Category } from '@svkm/db-storage';
 import {
   SWAGGER_CATEGORY_ACTIVE,
@@ -7,21 +9,31 @@ import {
   SWAGGER_CATEGORY_ID,
   SWAGGER_CATEGORY_NAME,
   SWAGGER_CATEGORY_SLUG,
+  toSlug,
 } from '@svkm/resources';
 
 export class CategoryDto {
   @ApiProperty(SWAGGER_CATEGORY_ID)
+  @IsNotEmpty({ message: 'ID is required | ID обязательно!' })
+  @IsString()
   id: string;
 
   @ApiProperty(SWAGGER_CATEGORY_SLUG)
+  @IsNotEmpty({ message: 'Slug is required | Slug обязателен!' })
+  @IsString()
+  @Transform(({ value: slug }) => toSlug(slug))
   slug: string;
 
   @ApiProperty(SWAGGER_CATEGORY_NAME)
+  @IsNotEmpty({ message: 'Необходимо указать name | название категории!' })
+  @IsString()
   name: string;
 
   @ApiProperty(SWAGGER_CATEGORY_DESCRIPTION)
+  @IsString()
   description: string;
 
+  // TODO add more
   @ApiProperty(SWAGGER_CATEGORY_ACTIVE)
   active: boolean;
 
